a99927416ad4599714ce06eafbaa86c5639e29ed
Tom Morton <tomm@riseup.net>
2023-05-01 10:00:34 +0100

    hi zoltan

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,8 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
+            <h1>HELLO ZOLTAN!!!!!!!</h1>
           </p>
           <div>
             <a
